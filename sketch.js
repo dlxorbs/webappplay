18b5b5c43a95f54eb88171a09c1c50223ef01ddd
taegyun <taegyun98@naver.com>
2022-11-17 21:32:26 +0900

    Main

--- sketch.js
+++ sketch.js
@@ -4,12 +4,18 @@
         D = 0.62, // Damping(감쇠)
         Sy = 0,
         Sx = 0,
-        Ex = 0,
-        Ey = 0, 
+        Ex = 600,
+        Ey = 400, 
         Ry = (Sy + Ey)/2,
-        Rx = (Sx +Ex)/2   
+        Rx = (Sx +Ex)/2,
+        vsx =0 ,
+        vsy =0,
+        asx = 0,
+        asy = 0,
+        fx =0 ,
+        fy =0, 
         psx = Rx,
-        psy = Ry ;
+        psy = Ry;
     
         let springHeight = 24,
         maxHeighty = 400,
@@ -26,25 +32,25 @@
 
 
 class Line {
-    constructor(Sx ,Sy,psx ,psy, Ex ,Ey){
+    constructor(Sx ,Sy,psx ,psy,  Ex ,Ey){
         this.Sx = Sx;
         this.Sy = Sy; 
         this.Ex = Ex; 
         this.Ey = Ey; 
         this.psx = psx; 
         this.psy = psy;
-        this.vsx = 0
-        this.asx = 0
-        this.fx = 0
-        this.vsy = 0
-        this.asy = 0
-        this.fy = 0
+        this.vsx = vsx
+        this.asx = asx
+        this.fx = fx
+        this.vsy = vsy
+        this.asy = asy
+        this.fy = fy
     }
 
     updateSpring() {
         // 용수철(spring) 위치 업데이트
         if ( !move ) {
-          this.fy = -K * ( psy - Ry ); // f=-ky
+          this.fy = -K * ( this.psy - Ry ); // f=-ky
           this.asy = this.fy / M;          // 가속도 설정, f=ma == a=f/m
           this.vsy = D * (this.vsy + this.asy);  // 속도 설정
           this.psy = psy + this.vsy;        // 업데이트된 위치
@@ -74,55 +80,73 @@ class Line {
         
         // 상단 막대기의 위치 설정 및 제한
         if (move) {
-          psx = mouseY - springHeight / 2;
-          psy = constrain(this.psy, minHeight, maxHeight);
+          this.psy = mouseY - springHeight / 2;
+          this.psy = constrain(this.psy, minHeight, maxHeight);
         }
         
         if (move) {
-          psx = mouseX - springHeight / 2;
-          psx = constrain(this.psx, minHeightx, maxHeightx);
+          this.psx = mouseX - springHeight / 2;
+          this.psx = constrain(this.psx, minHeightx, maxHeightx);
         }
       }
       
 
 
-    line(){
-        bezier( this.Sx ,this.Sy, this.psx, this.psy, this.Ex ,this.Ey);
-        strokeWeight(20);
+    drawline(){
+        if (over || move) {
+            stroke(255);
+          } else {
+            stroke(204);
+          }
+        bezier( this.Sx ,this.Sy, this.psx, this.psy, this.psx, this.psy, this.Ex ,this.Ey);
+        console.log(this.Ex)
+        strokeWeight(14)
+
     }
+
+
         
 
+    mouseDragged() {
+        if (over) {
+        move = true;
+        }
+        console.log(abs(sqrt((ballx-mouseX)**2+(mouseY-bally)**2)))
+    }
+    
+     mouseReleased() {
+        move = false;
+    }
+
 }
 
 
-let line = []
+let line = [
+
+]
 function setup() {
-    createCanvas(600, 400);
+    createCanvas(1200, 800);
     rectMode(CORNERS);
     noStroke();
-    line[0] = new Line(0,0, psx , psy , 600,400);
+    strokeWeight(10);
 }
+
+
 function draw() {
+    line[0] = new Line(Sx,Sy, psx , psy, Ex,Ey);
     ballx = mouseY*(width/height);
     bally = mouseX*(height/width);
     background(102);
 
-    ellipse(Sx, Sy, 20,20);
-    ellipse(Ex, Ey, 20,20);
     line[0].updateSpring();
-    line[0].line();
-
-    if (over || move) {
-        fill(255);
-      } else {
-        fill(204);
-      }
+    line[0].drawline();
+    line[0].mouseDragged();
+    line[0].mouseReleased();
     
-
     
+      console.log(Line)
     ellipse(ballx, bally, 20,20);
     ellipse(mouseX, mouseY, 20,20);
     // ellipse(ballx, bally, 20,20);
   }
 
-  console.log()
